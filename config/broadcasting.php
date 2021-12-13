0b2abcf9414fb54b2bbc94e4657cdef621297b91
doyoque <abiyoga1335@gmail.com>
2021-12-13 11:33:07 +0700

    remove host url pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,6 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => false,
-                'host' => env('PUSHER_APP_HOST'),
             ],
         ],
 
